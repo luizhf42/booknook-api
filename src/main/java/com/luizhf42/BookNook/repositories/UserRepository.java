feb2642aee7453426eb78a9725ad9a6c0fb4250d
Luiz Henrique <luizhf42@gmail.com>
2023-03-29 17:42:53 -0300

    feat: add user repository

---
+++ src/main/java/com/luizhf42/BookNook/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.luizhf42.BookNook.repositories;
+
+import com.luizhf42.BookNook.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
