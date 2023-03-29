176810f1aa8be82fa588ebfc96c59b76a9c1298c
Luiz Henrique <luizhf42@gmail.com>
2023-03-29 17:40:40 -0300

    feat: add SecurityConfig

---
+++ src/main/java/com/luizhf42/BookNook/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.luizhf42.BookNook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests().anyRequest().permitAll().and().cors().and().csrf().disable().build();
+    }
+}
