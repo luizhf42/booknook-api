10eaaa761d969fdb8b5035824bdf8047f07fc1ea
Luiz Henrique <luizhf42@gmail.com>
2023-04-24 17:17:35 -0300

    feat: add existsByUsername query method

--- src/main/java/com/luizhf42/BookNook/repositories/UserRepository.java
+++ src/main/java/com/luizhf42/BookNook/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.luizhf42.BookNook.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByUsername(String username);
 }
