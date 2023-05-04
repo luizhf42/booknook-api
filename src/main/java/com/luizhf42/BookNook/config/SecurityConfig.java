d36d24ffe626f5e551cb631993bd94350b83f385
Luiz Henrique <luizhf42@gmail.com>
2023-05-04 17:34:30 -0300

    fix: enable cors and csrf

--- src/main/java/com/luizhf42/BookNook/config/SecurityConfig.java
+++ src/main/java/com/luizhf42/BookNook/config/SecurityConfig.java
@@ -11,6 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests().anyRequest().permitAll().and().cors().and().csrf().disable().build();
+        return http.authorizeHttpRequests().anyRequest().permitAll().and().cors().and().csrf().and().build();
     }
 }
